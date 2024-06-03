772df04c058164b5c6f6020d6e8b0db0a662aa2e
Neeraj Kumar <nk4976744@gmail.com>
2024-06-03 17:56:53 +0530

    updated Readme

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+
+export default {
+    schema: "./utils/schema.jsx",
+    driver: 'pg',
+    dbCredentials: {
+      connectionString: 'postgresql://accounts:ypvN2ke3PGFE@ep-weathered-heart-a58wmzem.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require',
+    }
+  };
